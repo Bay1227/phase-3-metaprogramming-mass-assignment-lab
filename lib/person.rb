0151ea8690e26999721f0efdd9708c03c690527f
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-09-16 14:11:15 -0700

    Mass assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
   # your code here
-end
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_width, :pant_length
+
+
+def initialize(attributes)
+  attributes.each do |key, value|
+      self.send(("#{key}="), value)
+    end 
+  end 
+end 
